6b678df850bb5756ab4a244aa60b738962d44b09
phamnguyen23174600015 <pknguyen.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-15 20:43:19 +0700

    hoan thanh lab 02

---
+++ lab02/bai1.py
@@ -0,0 +1,5 @@
+thang = int(input("Nhập vào một tháng (1-12): "))
+if thang == 1 or thang == 3 or thang == 5 or thang == 7 or thang == 8 or thang == 10 or thang == 12:
+    print("Tháng có có 31 ngày.")
+elif thang == 4 or thang == 6 or thang == 9 or thang == 11:
+    print("Tháng có 30 ngày")
