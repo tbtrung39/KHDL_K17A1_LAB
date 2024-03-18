91b899dbf38aa2a4dc68545d9bfa69852423aaa5
Khoa hoc DU Lieu 17A1 <nnvinh.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-18 22:50:13 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/10.py
@@ -0,0 +1,6 @@
+print("chuong trinh tinh xac suat khi tung n lan co it nhat 1 lan ca ra ra 6")
+n=int(input("nhap so lan tung xuc sac: "))
+kha_nang_ba_mat_deu_ra_6=1/(6*6*6)
+ty_le=n*kha_nang_ba_mat_deu_ra_6
+ty_le=round(ty_le,2)
+print("khi tung {} lan ty le ca 3 xuc sac ra 6 la: ".format(n), ty_le)
