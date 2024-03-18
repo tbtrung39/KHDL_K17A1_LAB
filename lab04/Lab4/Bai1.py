367d43c75102dbeb5cfc2472f73408e31f4878e8
Nguyen Tien Dat <nguyendat062506@gmail.com>
2024-03-18 23:25:42 +0700

    Nguyen_Tien_Dat_23174600008/Hoan_Thanh_Lab04

---
+++ lab04/Lab4/Bai1.py
@@ -0,0 +1,24 @@
+# a
+n = int(input("Nhap n: "))
+i = 1
+tong = 0
+while i <= n:
+    tong += i**2
+    i += 1
+print(tong)
+# b
+n = int(input("Nhap n: "))
+i = 0
+tong = 0
+while i <= n:
+    tong += (2 * i + 1) ** 3
+    i += 1
+print(tong)
+# c
+n = int(input("Nhap n: "))
+i = 1
+tong = 0
+while i <= n:
+    tong += (2 * i) ** 4
+    i += 1
+print(tong)
