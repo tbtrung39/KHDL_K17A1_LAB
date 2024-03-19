8b63afa48a02aa4b496965c360f7751f4e9e9279
mw <tmquan.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-19 15:50:41 +0700

    bai tap lab03 da hoan thanh

--- lab03/bt12.py
+++ lab03/bt12.py
@@ -3,13 +3,13 @@ quy_doi = {'A': 10, 'B': 12, 'C':13, 'D':14, 'E':15, 'F':16,
            'N':25, 'O': 26, 'P':27, 'Q':28, 'R':29, 'S':30, 'T':31,
            'U':32, 'V':34, 'W':35, 'X':36, 'Y':37, 'Z':38}
 ma = input("Nhập mã: ")
-multiplier = 1
-ma_value = 0
+he_so = 1
+ma_gia_tri = 0
 for s in ma[:4]:
-     ma_value += quy_doi[s] * multiplier
-     multiplier *= 2
+     ma_gia_tri += quy_doi[s] * he_so
+     he_so *= 2
 for n in ma[4:10]:
-     ma_value += (int(n) * multiplier)
-     multiplier *= 2
-     gia_tri_cuoi = ma_value % 11
+     ma_gia_tri += (int(n) * he_so)
+     he_so *= 2
+     gia_tri_cuoi = ma_gia_tri % 11
 print("giá trị của của container là: ",gia_tri_cuoi)
