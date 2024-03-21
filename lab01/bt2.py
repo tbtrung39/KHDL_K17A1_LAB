ad2dade3ea0ac4dc46b7257bb8104579b78f5435
mw <tmquan.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-21 20:21:59 +0700

    bt lab01 da hoan thanh

---
+++ lab01/bt2.py
@@ -0,0 +1,8 @@
+d = int(input("Nhập vào số ngày: "))
+h = int(input("Nhập vào số giờ: "))
+m = int(input("Nhập vào số phút: "))
+s = int(input("Nhập vào số giây: "))
+ds = d * 86400 
+hs = h * 3600
+ms = m * 60
+print(f"Thời gian sau khi quy đổi ra giây {d} ngày = {ds}s, {h} giờ = {hs}s , {m} phút = {ms}s")
---
+++ lab01/bt5.py
@@ -0,0 +1,6 @@
+a1 = int(input("Nhập toạ độ vectơ: "))
+a2 = int(input("Nhập toạ độ vectơ: "))
+b1 = int(input("Nhập toạ độ vectơ: "))
+b2 = int(input("Nhập toạ độ vectơ: "))
+tvh = a1*b1 + a2*b2
+print("Tích vô hướng là: ", tvh)
