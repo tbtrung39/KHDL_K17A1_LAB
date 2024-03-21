731832f02021e05516544ac3792abb6fdfa0608d
mw <tmquan.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-21 16:42:40 +0700

    bai tap lab04 da hoan thanh

--- lab04/bt7.py
+++ lab04/bt7.py
@@ -1,13 +1,10 @@
 m = int(input("Nhập giá trị m: "))
 n = int(input("Nhập giá trị n: "))
+bcnn = m
 if m < n:
-    p = m;m = n;n = p
-while m != n:
-    q = m - n
-    if q > n: m = q
-    else:
-        m = n
-        n = q
-ucln = m
-bcnn = (m*n) // ucln
+    temp = m ; m = n ; n = temp 
+while True:
+    if bcnn % m == 0 and bcnn % n == 0:
+        break
+    bcnn += 1
 print(f"Bội chung nhỏ nhất là ",bcnn)
