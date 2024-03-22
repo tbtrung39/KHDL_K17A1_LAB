f93ef1c594fadee8c9bc1ffa42067aa20fe22028
dothinguyen17a1hn <148058042+dothinguyen@users.noreply.github.com>
2024-03-22 22:35:12 +0700

    thuc hanh lab02 da hoan thanh

---
+++ lab02/2.7.py
@@ -0,0 +1,11 @@
+diem_tk = float(input("Nhap vao diem tong ket : "))
+if diem_tk >= 0.0 and diem_tk < 3.0:
+    print("loai kem")
+elif diem_tk == 4.0:
+    print("loai yeu")
+elif diem_tk >= 5.0 and diem_tk < 6.0:
+    print("loai trung binh")
+elif diem_tk >=7.0 and diem_tk < 8.0:
+    print("loat kha")
+elif diem_tk >= 9 and diem_tk <= 10:
+    print("loai gioi")
