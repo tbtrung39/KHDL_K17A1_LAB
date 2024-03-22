626feac7e14b69187f5996eb1775ee0025a387a6
Nguyen_Thanh_Xuan_23174600025 <ntxuan.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-22 22:36:10 +0700

    thuc hanh lab01 da hoan thanh

--- lab02/1.py
+++
@@ -1,9 +0,0 @@
-thang=int(input("Nhap ten thang:"))
-if thang ==1 or thang==3 or thang==5 or thang==7 or thang==8 or thang==10 or thang==12:
-    print("Thang",thang ,"co 31 ngay")
-elif thang==2:
-    print("Thang 2 co 28 ngay hoac 29 ngay")
-elif thang==4 or thang==6 or thang==9 or thang==11:
-    print("thang",thang,"co 30 ngay")
-else:
-    print("nhap lai thang hop le")
