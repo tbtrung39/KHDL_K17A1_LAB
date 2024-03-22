e02750c6c2c3a9c45a45c6371a3b7e7d59c84c25
Khoa hoc du lieu 17A1 <ldquan.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-22 22:52:45 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/cau9.py
@@ -0,0 +1,4 @@
+n = int(input('nhap so lan tung xuc sac: '))
+so_lan_tung_ca_3_khong_ra_6 =(5/6)**n
+so_lan_tung_ca_3_ra_6 = 1 - so_lan_tung_ca_3_khong_ra_6
+print(f'xac suat tung 3 xuc sac ra 6 qua {n} lan la: {so_lan_tung_ca_3_ra_6:.2f}')
