eea0c9bd8244f04fad601ffb2553e83413ba2ca6
chungduong <dtchung.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-24 21:13:30 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/bai2.py
@@ -0,0 +1,3 @@
+x=int(input("nhập giá trị của x : "))
+bieuthuc=(-x+(x**2+4)**1/2)/(x**4+1)**1/7
+print("giá trị biểu thức = ", bieuthuc)
---
+++ lab01/bai8.py
@@ -0,0 +1,4 @@
+import math
+x=int(input("nhập x : "))
+bieuthuc= [math.log(x,4)+math.log(2,x)]
+print(bieuthuc)
