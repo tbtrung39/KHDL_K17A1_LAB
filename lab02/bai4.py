25c1955dfce5e32eebc1b50826b787309fd38690
Khoa hoc du lieu 17A1 <dtthu.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-27 23:29:14 +0700

    da hoan thanh lab02

--- lab02/bai4.py
+++
@@ -1,7 +0,0 @@
-so_nguyen = int(input("Nhập vào một số nguyên: "))
-
-if 100 <= so_nguyen <= 999:
-    hang_tram = so_nguyen // 100
-    print("Chữ số hàng trăm của số", so_nguyen, "là:", hang_tram)
-else:
-    print("Số không có chữ số hàng trăm. Xuất ra 0.")
