6e57c2b3eff07e0723aca0e2cdef412203a3ee4f
17a1 <dtdat.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-28 08:56:39 +0700

    thuc hanh lab04

---
+++ lab03/bt4.py
@@ -0,0 +1,11 @@
+n=int(input('nhập số n:'))
+if n<2:
+    print('không có số nguyên tố nào')
+else: 
+ print('số nguyên tố là:',end='')
+ for i in range(2,n):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        print(i,end=' ')
