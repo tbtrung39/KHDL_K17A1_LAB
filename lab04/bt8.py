41cecc075ce72bb18fbc2f47b5760468c1dca4fb
17a1 <dtdat.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-28 08:33:53 +0700

    xoa lab04

--- lab04/bt8.py
+++
@@ -1,3 +0,0 @@
-char = input("Nhập một ký tự: ")
-avalue = ord(char)
-print(f"Giá trị ASCII của ký tự '{char}' là: {avalue}")
--- lab04/bt9.py
+++
@@ -1,6 +0,0 @@
-so = input("Nhập số từ bàn phím: ")
-tong = 0
-for i in so:
-    tong += int(i)
-
-print(f"tổng của các chữ số đã nhập là {tong}")
