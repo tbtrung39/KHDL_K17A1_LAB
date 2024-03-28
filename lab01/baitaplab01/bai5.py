daeb2b3999f5cd048398eebeda684196d97b1c55
cuongnguyen0610 <nmcuong.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-28 18:15:37 +0700

    hoanthanhlab01

---
+++ lab01/baitaplab01/bai5.py
@@ -0,0 +1,6 @@
+a1 = int(input("Nhập vào a1:"))
+a2 = int(input("Nhập vào a2:"))
+b1 = int(input("Nhập vào b1:"))
+b2 = int(input("Nhập vào b2:"))
+result = a1*b1+a2*b2
+print("Tích vô hướng:",result)
