401bcdb4e9786aa1c31fd0183f074001af0bb9fe
K17A1 - DHKL <pdkhai.dhkl17a1hn@sv.uneti.edu.vn>
2024-03-31 14:26:10 +0700

    thuc hanh lab 04 hoan thanh

---
+++ lab04/Khai_lab04/bai 10.py
@@ -0,0 +1,25 @@
+a=input('nhập số:')
+i=0
+while i<len(a):
+    b=a[i]
+    if b=='1':
+        print('mot',end=' ')
+    if b=='2':
+        print('hai',end=' ')
+    if b=='3':
+        print('ba',end=' ')
+    if b=='4':
+        print('bon',end=' ')
+    if b=='5':
+        print('nam',end=' ')
+    if b=='6':
+        print('sau',end=' ')
+    if b=='7':
+        print('bay',end=' ')
+    if b=='8':
+        print('tam',end=' ')
+    if b=='9':
+        print('chin',end=' ')
+    if b=='0':
+        print('khong',end=' ')
+    i+=1
