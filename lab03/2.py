cf42e90c1dfb29e440ffb4eac230ac2e46ba2133
Khoa học dữ liệu <73875455+agnes292@users.noreply.github.com>
2024-04-01 22:13:21 +0700

    thuc hien lap03 hoan thanh

---
+++ lab03/2.py
@@ -0,0 +1,8 @@
+n=int(input("nhập số nguyên dương n: "))
+for i in range(1,n):
+    tong_uoc=0
+    for j in range(1,i):
+        if i %j==0:
+            tong_uoc+=j
+    if tong_uoc==i:
+        print(i,'',end='')
