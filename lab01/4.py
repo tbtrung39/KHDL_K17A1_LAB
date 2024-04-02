56d28f1f5708154fbfd9808666b1f9f9bc41928f
Nguyen_Thanh_Xuan_23174600025 <ntxuan.dhkl17a1hn@sv.uneti.edu.vn>
2024-04-02 11:15:13 +0700

    Thuc hanh lab01 da hoan thanh

---
+++ lab01/4.py
@@ -0,0 +1,9 @@
+u=220
+i=2.7
+t=float(input("Nhap thoi gian su dung:"))
+h=t/3600
+p=u*i*h
+print(f'Cong suat tieu thu la',p)
+doi_dv=p/1000
+tien_dien=doi_dv*7000
+print(f'so tien phari tra:',tien_dien)
---
+++ lab01/5.py
@@ -0,0 +1,6 @@
+a=float(input("Nhap gia tri a:"))
+b=float(input("Nhap gia tri b:"))
+c=float(input("Nhap gia tri c:"))
+x=-b/(2*a)
+y=a*x**2+b*x+c
+print(f'dinh cua phuong trinh la {x:.2f},{y:.2f}')
