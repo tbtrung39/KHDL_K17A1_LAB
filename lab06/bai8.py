5faf1f9919a8642deb678637b751a549c846f80e
khoa hoc du lieu <dtchung.dhkl17a1hn@sv.uneti.edu.vn>
2024-04-07 20:19:08 +0700

    thuc hanh lab05 da hoan thanh

--- lab06/bai8.py
+++
@@ -1,7 +0,0 @@
-Str = input("Nhập đoạn văn bản hoàn chỉnh: ")
-vanban = Str.split()
-tong = 0
-for tu in vanban:
-    if len(tu) == 1:
-        tong += 1
-print("Số lượng từ đơn trong đoạn văn bản là:", tong)
