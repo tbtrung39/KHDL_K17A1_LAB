37d08b86dd17ece2cd248ad85d00d6a5acf1f67b
Khoa hoc Du lieu 17A1 <nxhuy.dhkl17a1hn@sv.uneti.edu.vn>
2024-04-07 15:40:00 +0700

    hoan thanh lab05

--- lab06/btap lab06/5.py
+++
@@ -1,9 +0,0 @@
-#viết chương trình sinh 1 dãy list A gồm 1000 số tự nhiên , nằm ngẫu nhiên trong khoảng[1,99999]
-import random
-
-danh_sach_A = []
-for i in range(1000):
-    so_ngau_nhien = random.randint(1, 99999)
-    danh_sach_A.append(so_ngau_nhien)
-
-print("Danh sách A:", danh_sach_A)
--- lab06/btap lab06/9.py
+++
@@ -1,5 +0,0 @@
-danh_sach = [2, 4, 6, 8, 10]
-for num in danh_sach:
-    assert num % 2 == 0, f"Số {num} không phải là số chẵn"
-
-print("Tất cả các số trong danh sách là số chẵn")
