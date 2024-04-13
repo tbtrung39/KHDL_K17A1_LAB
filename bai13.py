a446b55fa73fd7dabfccb50515069182a60905dd
17A1_Quang_065 <148097482+38Quang17A1@users.noreply.github.com>
2024-04-13 18:30:38 +0700

    Delete bai13.py

--- bai13.py
+++
@@ -1,8 +0,0 @@
-cn = ["Anh", "Em"]
-dt = ["Chơi", "Yêu"]
-tn = ["Bóng đá", "Bóng rổ"]
-for mot in cn:
-    for hai in dt:
-        for ba in tn:
-            cau = f"{mot} {hai} {ba}"
-            print(cau)
