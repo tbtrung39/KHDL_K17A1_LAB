be550221f290f4bdf6a5a31aa3272924c2abbf24
Khoa hoc du lieu 17A1 <ldquan.dhkl17a1hn@sv.uneti.edu.vn>
2024-05-14 22:03:57 +0700

    thuc hanh lab08 da hoan thanh

---
+++ lab08/11.py
@@ -0,0 +1,6 @@
+def a():
+    q=input('nhập tên:')
+    t=float(input('nhập điểm toán:'))
+    l=float(input('nhập điểm lý:'))
+    h=float(input('nhập điểm hóa:'))
+    print(f'điểm trung bình:{(t+l+h)/3}')
