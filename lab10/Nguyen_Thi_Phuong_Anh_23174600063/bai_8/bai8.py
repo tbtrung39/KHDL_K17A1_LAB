2fada9c49834aeaaea0acaa1dec8022372e4cc95
Nguyen Thi Phuong Anh <ntpanh.dhkl17a1hn@sv.uneti.edu.vn>
2024-05-24 20:24:01 +0700

    hoàn thành lab10

---
+++ lab10/Nguyen_Thi_Phuong_Anh_23174600063/bai_8/bai8.py
@@ -0,0 +1,13 @@
+import pack_bai8.Matranvuong as b8
+
+n = int(input("Nhập kích thước của ma trận vuông: "))
+
+ma_tran = b8.nhap_ma_tran(n)
+b8.in_ma_tran(ma_tran)
+print("Ma trận chuyển vị là:")
+b8.in_ma_tran(b8.ma_tran_chuyen_vi(ma_tran))
+
+if b8.kiem_tra_ma_tran_doi_xung(ma_tran):
+    print("Ma trận là ma trận đối xứng.")
+else:
+    print("Ma trận không phải là ma trận đối xứng.")
