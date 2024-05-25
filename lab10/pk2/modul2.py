f6b747a9ba45c9e95648f2e613df5c54b56cd0df
khoa hoc du lieu 17A1 <tdhung.dhkl17a1hn@sv.uneti.edu>
2024-05-25 11:01:53 +0700

    Đã Hoàn thành lab10

---
+++ lab10/pk2/modul2.py
@@ -0,0 +1,5 @@
+def ChuviHinhvuong(a):
+    return 4 * a
+
+def DientichHinhvuong(a):
+    return a * a
---
+++ lab10/pk9/__init__.py
@@ -0,0 +1,1 @@
+from .qlycuahang import nhap_thong_tin_hang_hoa,sap_xep_theo_thue,in_hang_hoa
