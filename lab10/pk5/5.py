aae86fdc1060af02f853e1e136d8eda057064b22
Khoa hoc du lieu 17A1 <tvsy.dhkl17a1hn@sv.uneti.edu.vn>
2024-05-26 16:09:09 +0700

    thuc hanh lab10 da hoan thanh

---
+++ lab10/pk5/5.py
@@ -0,0 +1,3 @@
+import pack5 as b5
+ 
+b5.chuyen_doi_he_co_so()
---
+++ lab10/pk7/7.py
@@ -0,0 +1,6 @@
+import packb7 as b7
+
+danh_sach_so = b7.sinh_day_so()  
+b7.liet_ke_so_nguyen_to_chia_het_cho_7(danh_sach_so)
+b7.tinh_tong_so_le(danh_sach_so)
+b7.kiem_tra_so_chinh_phuong(danh_sach_so)
