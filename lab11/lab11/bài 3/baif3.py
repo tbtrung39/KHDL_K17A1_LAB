ac42bb9352b2de3d528257ed7fafd50ccd389053
khoa hoc du lieu 17A1 <tdhung.dhkl17a1hn@sv.uneti.edu>
2024-05-31 23:30:04 +0700

    đã hoàn thành lab11

---
+++ lab11/lab11/bài 3/baif3.py
@@ -0,0 +1,14 @@
+with open("f_in.dat",mode="r",encoding="utf-8") as file_in:
+       list_dayso = list(map(int,file_in.readline().strip().split()))
+list_so_cuc_tri = [];
+quantity = 0;
+for i in range(1,len(list_dayso)):
+       if list_dayso[i-1]< list_dayso[i]>list_dayso[i+1]:
+              list_so_cuc_tri.append(list_dayso[i])
+              quantity+=1
+with open("f_out.dat",mode="w") as file_out:
+       chain_cuc_tri = " ".join(map(str,list_so_cuc_tri))
+       file_out.write(str(quantity)+"\n")
+       file_out.write(chain_cuc_tri)
+
+              
