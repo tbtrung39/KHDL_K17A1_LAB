03210af06553d4cf7faf8062c54b19a700e2e763
khoa hoc du lieu 17A1 <148135338+NguyenPhThao23174600016@users.noreply.github.com>
2024-06-02 20:29:12 +0700

    da hoan thanh lab12

---
+++ lab12/b3.py
@@ -0,0 +1,13 @@
+filename = input("Nhập tên tệp: ")
+try:
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+except FileNotFoundError:
+    print("Lỗi: Tệp không tồn tại.")
+
+try:
+    with open('Copy.dat', 'w', encoding='utf-8') as copy_file:
+        copy_file.write(content)
+        print("Nội dung đã được sao chép vào Copy.dat")
+except Exception as e:
+    print(f"Lỗi khi ghi tệp: {e}")
