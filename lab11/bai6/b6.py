d12e764539fd9a38553f767004852787dfdccf44
Khoa hoc DU Lieu 17A1 <nnvinh.dhkl17a1hn@sv.uneti.edu.vn>
2024-06-02 12:51:12 +0700

    da hoan thanh lab11

---
+++ lab11/bai6/b6.py
@@ -0,0 +1,31 @@
+import csv
+with open(file = r"lab11\bai6\matran.txt",mode = "r") as f:
+    filedata = csv.reader(f)
+    list_item = list(filedata)
+    print(list_item[0])
+    print(list_item[2])
+    print(list_item)
+    odd = []
+    for i in list_item:
+        b=[]
+        for s in i:
+            if int(s) %2 !=0:
+                b.append(s)
+            else:
+                b.append(0)
+        if len(b)<4:
+            for i in range(4-len(b)):
+                b.append(0)
+        odd.append(b)
+with open(file = r"lab11\bai6\ODD.txt",mode = "w",newline="") as writer:
+    writer_file = csv.writer(writer)
+    for i in odd:
+        writer_file.writerow(i)
+#d
+with open(file = r"lab11\bai6\ODD.txt",mode = "r") as read:
+    read_file = csv.reader(read)
+    list_item = list(read_file)
+    print("dong cuoi file ODD.txt la:",list_item[-1])
+
+
+
